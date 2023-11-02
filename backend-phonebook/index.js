9b03e9eb452da0cfc196834d005b43449ba70e62
AcidicSoil <claytonbivens1@gmail.com>
2023-11-02 10:23:44 -0500

    added proxy so frontend can see backend

--- backend-phonebook/index.js
+++ backend-phonebook/index.js
@@ -1,17 +1,8 @@
 const express = require("express");
-const morgan = require("morgan");
 const app = express();
-
-// Middleware for parsing JSON
 app.use(express.json());
 
-// Morgan for logging
-morgan.token("body", (req, res) => JSON.stringify(req.body));
-app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
-
-const phonebookEntries = [
+let phonebookEntries = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
   { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
   { id: 3, name: "Dan Abramov", number: "12-43-234345" },
@@ -23,7 +14,20 @@ app.get("/api/persons", (req, res) => {
   res.json(phonebookEntries);
 });
 
-// Route to add a new phonebook entry
+// Route to retrieve information for the /info page
+app.get("/info", (req, res) => {
+  const numberOfEntries = phonebookEntries.length;
+  const currentTime = new Date().toISOString();
+  res.send(`
+    <p>Phonebook has info for ${numberOfEntries} people</p>
+    <p>Server time at the request: ${currentTime}</p>
+  `);
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(phonebookEntries);
+});
+
 app.post("/api/persons", (req, res) => {
   const body = req.body;
 
